3c9669c32dd6f9c4f1ce6a021d47a9534fd3b5c7
Ava <avamsteimle@gmail.com>
2022-03-09 09:21:41 -0800

    typeDefs for saveBook mutation

--- server/schemas/Resolvers.js
+++ server/schemas/Resolvers.js
@@ -1,18 +1,31 @@
-const { AuthenticationError } = require('apollo-server-express');
+const { AuthenticationError, UserInputError } = require('apollo-server-express');
 const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
         users: async () => {
-            return User.find();
+            return User.find().populate('books');
           },
         user: async (parent, { username }) => {
-          return User.findOne({ username });
+          return User.findOne({ username }).populate('books');
         },
       },
-    
+      Mutation: {
+        createUser: async (parent, args) => {
+          try { 
+            // Create new user
+            const user = await User.create(args);
+            // Create token 
+            return { user, token: "fake_token" }
+            // Return { token, user } or throw error
+          } catch (err) {
+            console.log(err);
+            throw UserInputError("username and email must be unique");
+          }
+      }
     }
+  }
 
 
 module.exports = resolvers;
--- server/schemas/TypeDefs.js
+++ server/schemas/TypeDefs.js
@@ -1,18 +1,40 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type Query {
+  users: [User]
+  books: [Book]
+  user(username: String!): User
+  me: User
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+type Mutation {
+  createUser(username: String!, email: String!, password: String!): Auth!
+  saveBook(authors: [String], description: String!, title: String!, bookId: String!, image: String, link: String): User!
+}
+
 type User {
     _id: ID!
     username: String!
     email: String!
-    password: String!
-  }
+    savedBooks: [Book]
+}
 
-  type Query {
-    users: [User]
-    user(username: String!): User
-    me: User
-  }
+type Book {
+    _id: ID!
+    authors: [String]
+    description: String!
+    BookId: String!
+    image: String
+    link: String
+    title: String!
+}
+  
 `;
 
 module.exports = typeDefs;
