bde17f850abd3415c7d4f3ee74d964d35e9798b9
Ava <avamsteimle@gmail.com>
2022-03-09 23:32:17 -0800

    mutations finised in client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -28,13 +28,35 @@ export const SAVE_BOOK = gql`
   mutation saveBook($authors: [String], $description: String!, $title: String!, $bookId: String!, $image: String, $link: String) {
     saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link) {
       _id
-      authors
-      description
-      title
-      bookId
-      image
-      link
+      username
+      email
+      savedBooks {
+          authors
+          description
+          title
+          bookId
+          image
+          link
+      }
     }
   }
 `;
 
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
