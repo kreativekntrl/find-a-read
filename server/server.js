3687c2822447668865b2ace4d27c975cbbc1fb7b
Ava <avamsteimle@gmail.com>
2022-03-10 18:39:56 -0800

    updating

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas/index');
+import { typeDefs, resolvers } from './schemas';
 const db = require('./config/connection');
 const routes = require('./routes');
 const { authMiddleware } = require('./utils/auth');
